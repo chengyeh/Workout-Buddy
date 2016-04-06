6cde6bdfec8c9c76d14e8b08469cfde8981d222d
Paul Charles <santosh@ku.edu>
2016-04-05 21:22:21 -0500

    Added public/private user functionality

--- add_group.php
+++ add_group.php
@@ -11,16 +11,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
   // Trim all the incoming data:
 	$trimmed = array_map('trim', $_POST);
-				
 
 
-		// Add the user to the database: 
+
+		// Add the user to the database:
 		$group = new Group();
-		$group->group_owner= $trimmed['user_id'];	
+		$group->group_owner= $trimmed['user_id'];
       	$group->group_name= $trimmed['group_name'];
-     
+      	$group->group_status=$trimmed['group_status'];
+
       	$group->create();
-      	
+
       	//Redirect to profile page
       	redirect_to("view_group.php?id={$database->insert_id()}");
 }
@@ -28,23 +29,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 <html>
 <head>
 	<title>Workout Buddy: Add Group</title>
-	
+
 </head>
 	<body>
 	<h1>Profile Page: Add Group</h1>
 	<p><a href="profile.php">Profile</a>|<a href="logout.php">logout</a></p>
-	
+
 	<h2>User Info</h2>
-	<?php 
+	<?php
 		echo "<p>User Name: " . $session->user_name. "</p>";
 		echo "<p>User Id: " . $session->user_id . "</p>";
 	?>
-	
+
 	<h2>Group Add Form</h2>
 	<form action="add_group.php" method="post" enctype="multipart/form-data">
 		 <input type="hidden" name="user_id" value='<?php echo $session->user_id; ?>'>
 		<label>Group Name</label>
 		<input type="text" name="group_name" required /><br/>
+		<input type="radio" name="group_status" value="Private">Private<br>
+		<input type="radio" name="group_status" value="Public">Public<br>
 		<button type="submit" name="submit">Add group</button>
 	</form>
 	</body>
--- includes/group.php
+++ includes/group.php
@@ -3,11 +3,12 @@ require_once(LIB_PATH.DS."database.php");
 
 class Group extends DatabaseObject {
    	protected static $table_name = "wb_group";
-    protected static $db_fields = array('id','group_name','group_owner');
+    protected static $db_fields = array('id','group_name','group_owner','group_status');
     public $id;
     public $group_name;
     public $group_owner;
-    
+    public $group_status;
+
     public function get_members(){
     	global $database;
     	$sql = "SELECT * FROM wb_group_members WHERE group_id=".$this->id;
@@ -15,4 +16,3 @@ class Group extends DatabaseObject {
     	return $group_member_array;
     }
 }
-
