77ad983460e7fb56ad0845469ebae1e151da73f3
Dilesh Fernando <d605f211@ku.edu>
2016-04-06 16:53:57 -0500

    Add template to add_grop file

--- add_group.php
+++ add_group.php
@@ -24,21 +24,93 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
       	redirect_to("view_group.php?id={$database->insert_id()}");
 }
 ?>
-<html>
-<head>
-	<title>Workout Buddy: Add Group</title>
-</head>
-	<body>
-	<h1>Profile Page: Add Group</h1>
-	<p><a href="profile.php">Profile</a>|<a href="logout.php">logout</a></p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-	<h2>User Info</h2>
-	<?php
-		echo "<p>User Name: " . $session->user_name. "</p>";
-		echo "<p>User Id: " . $session->user_id . "</p>";
-	?>
+    <title>Fixed Top Navbar Example for Bootstrap</title>
 
-	<h2>Group Add Form</h2>
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="dist/css/navbar-fixed-top.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!--  <a class="navbar-brand" href="#">Project name</a> -->
+          <img id="navbar-logo-image" alt="workout buddy logo" src="images/Workout_Buddy_Logo_small.png">
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="profile.php">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More Actions<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="add_group.php">Add Group</a></li>
+                <li><a href="find_group.php">Find Group</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="addChallenges.php">Add Challenge</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          
+          <ul class="nav navbar-nav navbar-right" id="navbar-status">
+            <li><span ><span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp Hi <?php echo $session->user_name; ?>!&nbsp &nbsp<a class="btn btn-primary btn-sm" href="logout.php" role="button">Logout</a></span>
+        </div><!--/.nav-collapse -->
+      </div>
+      
+    </nav>
+
+    <div class="container">
+
+	<h2>Group Add</h2>
+	<form>
+		<fieldset class="form-group">
+		   <label for="formGroupExampleInput">Example label</label>
+		   <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+		</fieldset>
+  		<fieldset class="form-group">
+    		<label for="formGroupExampleInput2">Another label</label>
+    		<input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+  		</fieldset>
+	</form>
 	<form action="#" method="post" enctype="multipart/form-data">
 		 <input type="hidden" name="user_id" value='<?php echo $session->user_id; ?>'>
 		<label>Group Name</label>
@@ -47,5 +119,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 		<input type="radio" name="group_status" value="Public">Public<br>
 		<button type="submit" name="submit">Add group</button>
 	</form>
-	</body>
+	</div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
