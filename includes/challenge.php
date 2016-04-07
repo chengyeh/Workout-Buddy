485a73c2faa7105a7b894552a6accae309693de6
KUEI-HSIEN <guixien@gmail.com>
2016-04-07 16:17:19 -0500

    added pu_top3() method; get top3 pull up members

--- includes/challenge.php
+++ includes/challenge.php
@@ -3,7 +3,7 @@ require_once(LIB_PATH.DS."database.php");
 
 class challenge extends DatabaseObject {
    	protected static $table_name = "challenge";
-    protected static $db_fields = array('id','who', 'name', 'bench_press', 'pull_ups');
+    protected static $db_fields = array('id','who', 'name', 'bench_press', 'pull_ups', 'treadmill_mileage');
     public $id;
     public $who; //retrieve the user_id from the user (unique);
     public $name; //the name the user wish to appear
@@ -11,14 +11,20 @@ class challenge extends DatabaseObject {
     public $pull_ups = 0; //pull ups (numbers)
 
 
-
-
     public static function bp_top3(){
       global $database;
-      $sql = "SELECT name, bench_press FROM challenge ORDER BY bench_press DESC LIMIT 3;"
+      $sql = "SELECT name, bench_press FROM challenge ORDER BY bench_press DESC LIMIT 3;";
       $bp_top3 = $database->query($sql);
       return $bp_top3;
     }
+
+    public static function pu_top3(){
+      global $database;
+      $sql = "SELECT name, pull_ups FROM challenge ORDER BY pull_ups DESC LIMIT 3;";
+      $pu_top3 =  = $database->query($sql);
+      return $pu_top3;
+    }
+
 
     public static function get_BP200(){
       global $database;
