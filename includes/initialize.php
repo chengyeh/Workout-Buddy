0dc14ea3bb81dc12a25c6669e8a976730464a068
Dilesh Fernando <d605f211@ku.edu>
2016-04-08 15:52:05 -0500

    Add inbox unread mail count to profile

--- includes/initialize.php
+++ includes/initialize.php
@@ -3,7 +3,7 @@ defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
 defined('SITE_ROOT') ? null :
 
-define('SITE_ROOT', DS.'home'.DS.'clee28'.DS.'public_html'.DS.'EECS448'.DS.'EECS448_Project3');
+define('SITE_ROOT', DS.'home'.DS.'dfernand'.DS.'public_html'.DS.'eecs448'.DS.'EECS448_Project3');
 
 defined('LIB_PATH') ? null :
 define('LIB_PATH', SITE_ROOT .DS. 'includes');
@@ -16,9 +16,9 @@ require_once(LIB_PATH.DS."session.php");
 require_once(LIB_PATH.DS."database.php");
 require_once(LIB_PATH.DS."database_object.php");
 // require_once(LIB_PATH.DS."pagination.php");
-require_once(LIB_PATH.DS."PHPMailer".DS."class.phpmailer.php");
-require_once(LIB_PATH.DS."PHPMailer".DS."class.smtp.php");
-require_once(LIB_PATH.DS."PHPMailer".DS."language".DS."phpmailer.lang-es.php");
+// require_once(LIB_PATH.DS."PHPMailer".DS."class.phpmailer.php");
+// require_once(LIB_PATH.DS."PHPMailer".DS."class.smtp.php");
+// require_once(LIB_PATH.DS."PHPMailer".DS."language".DS."phpmailer.lang-es.php");
 
 require_once(LIB_PATH.DS."user.php");
 require_once(LIB_PATH.DS."group.php");
--- profile.php
+++ profile.php
@@ -92,9 +92,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
               </ul>
             </li>
           </ul>
-
+			
           <ul class="nav navbar-nav navbar-right" id="navbar-status">
-            <li><span ><span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp Hi <?php echo $session->user_name; ?>!&nbsp &nbsp<a class="btn btn-primary btn-sm" href="logout.php" role="button">Logout</a></span>
+            <li><span ><a href="inbox.php">Inbox </a>&nbsp
+            
+            <?php 
+            	$result_set = $database->query("SELECT * FROM wb_messages WHERE 'read'=0 AND receiver=".$user->id);
+            	$number_messages = $database->num_rows($result_set);
+            	echo "<span class='badge'>{$number_messages}</span>";
+            ?>
+            
+            &nbsp &nbsp &nbsp &nbsp<span class="glyphicon glyphicon-user" aria-hidden="true"></span> &nbsp Hi <?php echo $session->user_name; ?>!&nbsp &nbsp<a class="btn btn-primary btn-sm" href="logout.php" role="button">Logout</a></span>
+   			
         </div><!--/.nav-collapse -->
       </div>
 
@@ -158,11 +167,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 			echo "</table>";
 		}
 	?>
-
-  <p><a href="addChallenges.php">Add Challenge</a>|<a href="view_challenges.php">View Challenge</a><p>
-
     </div> <!-- /container -->
-
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -172,8 +177,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <script src="dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-
-
-
 	</body>
 </html>
