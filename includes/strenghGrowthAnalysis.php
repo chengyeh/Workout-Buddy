68cb4f411cce68d85f1252931cb8d8315d94b837
KUEI-HSIEN <guixien@gmail.com>
2016-04-08 17:33:05 -0500

    SGA in construction

--- includes/strenghGrowthAnalysis.php
+++ includes/strenghGrowthAnalysis.php
@@ -3,23 +3,26 @@ require_once(LIB_PATH.DS."database.php");
 
 class strenghGrowthAnalysis extends DatabaseObject {
     protected static $table_name = "strenghGrowthAnalysis";
-    protected static $db_fields = array('id', 'who', 'bench_press_imp', 'bench_press_previous', 'pull_up_imp', 'pull_up_previous', 'treadmill_imp', 'treadmill_previous');
+    protected static $db_fields = array('id', 'who', 'bench_press_imp', 'bench_press_now', 'bench_press_previous', 'pull_up_imp', 'pull_up_now','pull_up_previous', 'treadmill_imp', 'treadmill_now', 'treadmill_previous');
 
     public $id;
     public $who;
 
     //how much the person's strength grow over a period of time
-    //imp stands for improvement; the unit for for this is diff_lbs/days; type float
+    //now stands for quantity of each category as of now
     //previous stands for what it was before
-    public $bench_press_imp = 0.0;
+    //imp stands for improvement; the unit for for this is diff_lbs/days; type float
+    public $bench_press_now = 0.0;
     public $bench_press_previous = 0.0;
+    public $bench_press_imp = 0.0;
     //how much the person's pull up number grow over a period of time
-    public $pull_up_imp = 0.0;
+    public $pull_up_now = 0.0;
     public $pull_up_previous = 0.0;
+    public $pull_up_imp = 0.0;
     //////how much the person's Treadmill mileage grow over a period of time
-    public $treadmill_imp = 0.0;
+    public $treadmill_now = 0.0;
     public $treadmill_previous = 0.0;
-
+    public $treadmill_imp = 0.0;
 }
 
 ?>
--- updateStrenghGrowthAnalysis.php
+++ updateStrenghGrowthAnalysis.php
@@ -15,19 +15,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   $trimmed = array_map('trim', $_POST);
 
   $sga = new strenghGrowthAnalysis();
-  $sql = "SELECT * FROM strenghGrowthAnalysis WHERE who LIKE '". $trimmed['user_id'] . "';";
+  $user = $trimmed['user_id'];
+  $sql = "SELECT * FROM strenghGrowthAnalysis WHERE who LIKE '". $user . "';";
 
   //if the user have not created a record before
-  if(mysqli_num_rows($sql) == 0){
+  /*
+    now the mysqli_num_rows seems to be always less than 1, or == 0, check it out later.
+  */
+  if(mysqli_num_rows($sql) > 0){
+    //if the user has created a record before
+      $sql = "SELECT * FROM strenghGrowthAnalysis WHERE bench_press_now != 0.0 AND who LIKE '". $trimmed['user_id'] . "';";
+      //if the user has updated before; anything that has _now must has value
+
+      //if the user has not been updated before; _now does not have value
+      if(mysqli_num_rows($sql) == 0){
+        $sga->who = $trimmed['user_id'];
+        $sga->bench_now = $trimmed['sga_BP_lbs'];
+        $sga->pull_up_now = $trimmed['sga_pull_up'];
+        $sga->treadmill_now = $trimmed['sga_tmm'];
+        $sga->update();
+      }
+  }
+  else{
     $sga->who = $trimmed['user_id'];
     $sga->bench_press_previous = $trimmed['sga_BP_lbs'];
     $sga->pull_up_previous = $trimmed['sga_pull_up'];
     $sga->treadmill_previous = $trimmed['sga_tmm'];
     $sga->create();
-  }
-  //if the user has created a record before
-  else{
-
   }
 
   //Redirect to profile page
@@ -57,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         <tr><td><label>Your current bench press (lbs):</label></td><td><input type="text" name="sga_BP_lbs" required /></td></tr>
         <tr><td><label>You current max numbers of pull ups (#):</label></td><td><input type="text" name="sga_pull_up" required /></td></tr>
         <tr><td><label>Your current longest run (miles):</label></td><td><input type="text" name="sga_tmm" required /></td></tr>
-        	<tr><td><button type="submit" name="submit">Update your strength growth! WooHoo</button></td><td></td></tr>
+        <tr><td><button type="submit" name="submit">Update your strength growth! WooHoo</button></td><td></td></tr>
       </table>
   </body>
 </html>
