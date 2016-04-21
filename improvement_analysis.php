7474218e38c302e2d03e0c6ea13ad736d2139690
KUEI-HSIEN <guixien@gmail.com>
2016-04-21 17:43:35 -0500

    Improvement analysis table view added

--- improvement_analysis.php
+++ improvement_analysis.php
@@ -18,9 +18,7 @@ function dateDiff ($d1, $d2) {
 //find user id
 $user = User::find_by_id($session->user_id);
 $user = $user->id;
-
-
-
+//Create User object from ID in the URL
 
 
 ?>
@@ -40,40 +38,54 @@ $user = $user->id;
 
     $result_1 = $database->query($sql_1);
 
-    //echo "<table><tr><td>".$user."has done the following:</td><td>Overall Improvement rate:</td><td>Between:</td></tr>";
+    echo "<table><tr><td> You have done the following:   </td><td>Your strengh is growing at:   </td><td>Between:    </td></tr>";
     while($row_1 = $result_1->fetch_assoc()){
-      $exercise_id = $row_1["exercise_type_id"];  // this will get different workout that the user have; type Int
+
+      echo "<tr>";
+      // this will get different workout that the user have; type Int
+      $exercise_id = $row_1["exercise_type_id"];
 
       //pull out the workout names
       $sql_2 = "SELECT name FROM `wb_exercise_type` WHERE id = '". $exercise_id. "';";
       $result_2 = $database->query($sql_2);
       $row_2 = $result_2->fetch_assoc();
-      echo $row_2["name"] . "<br>";
+      echo "<td>".  $row_2["name"] ."</td>";
 
       //pull out the minimum weight (weakest workout) record
       $sql_3 = "SELECT MIN(weight), date FROM `wb_user_log` WHERE (exercise_type_id = '". $exercise_id . "' ) AND (user_id = '". $user ."');";
       $result_3 = $database->query($sql_3);
       $row_3 = $result_3->fetch_assoc();
-      echo "Min weight: " . $row_3["MIN(weight)"] . "<br>";
-      echo "Min weight date: " .$row_3["date"] . "<br>";
-
-      $d1 = $row_3["date"];
-      $d1 = str_replace('-', '/', $d1); // change format
-      $d1 = strtotime($d1);
+      $min_weight = $row_3["MIN(weight)"];
 
       //pull out the maximum weight (weakest workout) record
       $sql_4 = "SELECT MAX(weight), date FROM `wb_user_log` WHERE (exercise_type_id = '". $exercise_id . "' ) AND (user_id = '". $user ."');";
       $result_4 = $database->query($sql_4);
       $row_4 = $result_4->fetch_assoc();
-      echo "Max weight: " . $row_4["MAX(weight)"] . "<br>";
-      echo "Max weight date: " .$row_4["date"] . "<br>";
+      $max_weight = $row_4["MAX(weight)"];
+
+      $d1 = $row_3["date"];
+      $d1 = str_replace('-', '/', $d1); // change format
+      $d1 = strtotime($d1);
 
       $d2 = $row_4["date"];
       $d2 = str_replace('-', '/', $d2);
       $d2 = strtotime($d2);
 
       $days_between = ceil(abs($d2 - $d1) / 86400);
-      echo "Days between: ". $days_between . "<br>";
+
+      $rate;
+      //improvement rate
+      if($days_between == 0){
+        $rate = $max_weight - $min_weight;
+        echo "<td>" . $rate . " lbs/day</td>";
+      }
+      else{
+        $rate = ($max_weight - $min_weight) / $days_between;
+        echo "<td>" . $rate . " lbs/day</td>";
+      }
+      //date
+      echo "<td>From: ".$row_3["date"]."<br>To: ".$row_4["date"]."</td>";
+
     }
 
 
