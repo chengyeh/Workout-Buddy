e55d978a7831e94be0c37e1d759071e3d17bb0cd
Dilesh Fernando <d605f211@ku.edu>
2016-04-21 23:46:12 -0500

    Add test page list exersice for testing pagination

--- includes/initialize.php
+++ includes/initialize.php
@@ -4,7 +4,7 @@ defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
 defined('SITE_ROOT') ? null :
 
-define('SITE_ROOT', DS.'home'.DS.'kchu25'.DS.'public_html'.DS.'eecs448'.DS.'EECS448_Project3');
+define('SITE_ROOT', DS.'home'.DS.'dfernand'.DS.'public_html'.DS.'eecs448'.DS.'EECS448_Project3');
 
 defined('LIB_PATH') ? null :
 define('LIB_PATH', SITE_ROOT .DS. 'includes');
@@ -16,7 +16,7 @@ require_once(LIB_PATH.DS."functions.php");
 require_once(LIB_PATH.DS."session.php");
 require_once(LIB_PATH.DS."database.php");
 require_once(LIB_PATH.DS."database_object.php");
-// require_once(LIB_PATH.DS."pagination.php");
+require_once(LIB_PATH.DS."pagination.php");
 // require_once(LIB_PATH.DS."PHPMailer".DS."class.phpmailer.php");
 // require_once(LIB_PATH.DS."PHPMailer".DS."class.smtp.php");
 // require_once(LIB_PATH.DS."PHPMailer".DS."language".DS."phpmailer.lang-es.php");
@@ -26,7 +26,7 @@ require_once(LIB_PATH.DS."group.php");
 require_once(LIB_PATH.DS."group_members.php");
 require_once(LIB_PATH.DS."message.php");
 require_once(LIB_PATH.DS."challenge.php");
-//require_once(LIB_PATH.DS."exercise.php");
+require_once(LIB_PATH.DS."exercises.php");
 require_once(LIB_PATH.DS."goals.php");
 require_once(LIB_PATH.DS."routine.php");
 require_once(LIB_PATH.DS."set.php");
---
+++ list_exercise.php
@@ -0,0 +1,182 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$number_messages=0;
+
+require_once('includes/initialize.php');
+if(!$session->is_logged_in()){ redirect_to("login.php"); }
+
+//Create User object
+$user = User::find_by_id($session->user_id);
+
+//get the current page number
+$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+
+//recodrs per page
+$per_page = 10;
+
+//total record count 
+//$total_count =  Exercises::count_all();
+
+$sql = "SELECT COUNT(*) FROM wb_exercise_type";
+$result_set = $database->query($sql);
+$row = $database->fetch_array($result_set);
+$total_count = array_shift($row);
+
+$pagination = new Pagination($page, $per_page, $total_count);
+
+$sql = "SELECT * FROM wb_exercise_type ";
+$sql .= "LIMIT {$per_page} ";
+$sql .= "OFFSET {$pagination->offset()}";
+		
+$exercises = $database->query($sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Fixed Top Navbar Example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="dist/css/navbar-fixed-top.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!--  <a class="navbar-brand" href="#">Project name</a> -->
+          <img id="navbar-logo-image" alt="workout buddy logo" src="images/Workout_Buddy_Logo_small.png">
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="profile.php">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More Actions<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="add_group.php">Add Group</a></li>
+                <li><a href="find_group.php">Find Group</a></li>
+                <li><a href="find_user.php">Find User</a></li>
+                <li><a href="message_menu.php">Messages</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="addChallenges.php">Add Challenge</a></li>
+                <li><a href="view_challenges.php">View Challenge</a></li>
+              </ul>
+            </li>
+          </ul>
+          
+          <ul class="nav navbar-nav navbar-right" id="navbar-status">
+            <li><span class="glyphicon glyphicon-calendar"><a href="show_calendar">Calendar</a></span>&nbsp&nbsp</li>
+            <li>
+            	<span>
+	            <?php
+	            	$result_set = $database->query("SELECT * FROM wb_messages WHERE 'read'!=0 AND receiver=".$user->id);
+	            	$number_messages = $database->num_rows($result_set);
+	            	echo "<span class='badge'>{$number_messages}</span>";
+	            ?>
+	            <a href="inbox.php">Inbox</a>
+	            </span>&nbsp&nbsp
+            </li>
+
+            <li><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Hi <?php echo $session->user_name; ?>&nbsp&nbsp</li>
+            <li><span><a class="btn btn-primary btn-sm" href="logout.php" role="button">Logout</a></span>&nbsp&nbsp</li>
+         </ul>
+         
+         </div><!--/.nav-collapse -->
+      </div>
+      
+    </nav>
+
+    <div class="container">
+
+    <!-- Main component for a primary marketing message or call to action -->
+  	
+  	<h2>List Exercise</h2>
+ <div>
+  	<?php 
+	 while ($row = mysqli_fetch_array($exercises)) 
+ {
+     echo $row['name']. "<br/>";
+ }
+ ?>
+ </div>
+ 
+ <div>
+ 
+ <?php
+ if($pagination->total_pages() > 1) {
+ 
+ 	if($pagination->has_previous_page()) {
+ 		echo "<a href=\"list_exercise.php?page=";
+ 		echo $pagination->previous_page();
+ 		echo "\">&laquo; Previous</a> ";
+ 	}
+ 
+ 	for($i=1; $i <= $pagination->total_pages(); $i++) {
+ 		if($i == $page) {
+ 			echo " <span class=\"selected\">{$i}</span> ";
+ 		} else {
+ 		echo " <a href=\"list_exercise.php?page={$i}\">{$i}</a> ";
+ 		}
+ 	}
+ 
+ 	if($pagination->has_next_page()) {
+ 	echo " <a href=\"list_exercise.php?page=";
+			echo $pagination->next_page();
+ 			echo "\">Next &raquo;</a> ";
+    }
+ 
+ 	}
+  	?>
+ </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
