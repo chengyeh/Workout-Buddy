ca0fdb014fe40ec550b390bf0a4af2714b4ba539
Dilesh Fernando <d605f211@ku.edu>
2016-04-30 08:42:25 -0500

    Add second test

--- test/database_test.php
+++ test/database_test.php
@@ -13,8 +13,8 @@ include('header.html');
 <h3 class="sub-header">Test close_connection()</h3>
 <h3 class="sub-header">Test query($sql)</h3>
 <?php 
-$database2 = new MySQLDatabase();
-$result = $database2->query("SELECT * FROM wb_users");
+$database3 = new MySQLDatabase();
+$result = $database3->query("SELECT * FROM wb_users");
 if($result){
 	echo "<div class='well' style='background-color: #b3ffcc'>";
 	echo "Test query(\$sql) PASSED";
@@ -24,8 +24,39 @@ if($result){
 	echo "Test query(\$sql) FAILED";
 	echo "</div>";
 }
+
+//Delete MySQLDatabase object
+unset($database3);
 ?>
 <h3 class="sub-header">Test escape_value($value)</h3>
+<?php 
+//Create instance of MySQLDatabase class
+$database4 = new MySQLDatabase();
+
+//Create test string with apostrophe.
+$test_string = "Joe O'Brien";
+
+//Call the escape_value method
+$result = $database4->escape_value($test_string);
+
+//Check result contains backslash charactor
+if (strpos($result, '\\') !== FALSE) {
+	//String contains backslash
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+	echo "Test escape_value(\$value) PASSED";
+	echo "</div>";
+	
+}else{
+	//String do not contain backslash
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+	echo "Test escape_value(\$value) FAILED";
+	echo "</div>";
+}
+
+//Delete MySQLDatabase object
+unset($database4);
+?>
+
 <h3 class="sub-header">Test fetch_array($result_set)</h3>
 <h3 class="sub-header">Test num_rows($result_set)</h3>
 <h3 class="sub-header">Test insert_id()</h3>
