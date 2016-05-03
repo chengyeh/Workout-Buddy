41edd840578e46a4895b832f5e30d986f488d818
Paul Charles <santosh@ku.edu>
2016-05-03 08:34:51 -0500

    Update exercise set tests with display exercise set and added new styling and format

--- test/exercise_set_test.php
+++ test/exercise_set_test.php
@@ -6,14 +6,12 @@ require_once('../includes/initialize.php');
 include('header.html');
 ?>
 <!-- html goes here -->
-<h1 class="page-header">Exercise Testing</h1>
-<h3 class="sub-header">Test for Adding Exercise Set to Exercise and Routine Exercises</h3>
-<p>This test will test if an exercise set is tied in with a routine and an exercise to be querried.</p>
-&nbsp;
-<p>Following code will test if the exercise set can be created.</p>
-<div class="well">
+<h1 class="page-header">Exercise Set Testing</h1>
+<h3 class="expand sub-header">Test for Adding Exercise Set to Exercise and Routine Exercises</h3>
+
+<div class="well" style="display:none;">
 <xmp>
-			$rout_ex_set = new Routine();
+	$rout_ex_set = new Routine();
 		    $rout_ex_set->user_id = 19;
 		    $rout_ex_set->name = "Test Routine for Set and Exercise";
 		    $rout_ex_set->description = "This Routine is for Testing Purposes";
@@ -34,21 +32,18 @@ include('header.html');
 			$test_set->order=1;
 			$test_set->reps=10;
 			$test_set->weight=30;
+			global $database;
+			$database->query("INSERT INTO `wb_exercise_set`(`exercise_id`, `routine_id`, `order`, `reps`, `weight`) VALUES ($test_set->exercise_id,$test_set->routine_id,$test_set->order,$test_set->reps,$test_set->weight)");
+			$test_set->id = $database->insert_id();
 
 
-   			global $database;
+			global $database;
 			$sql = "SELECT * FROM wb_exercise_set WHERE routine_id=".$rout_ex_set->id." AND exercise_id=".$ex_set->id;
 			$ex_set_test_array = $database->query($sql);
 			$pass_trigger=0;
 	    	while($test_set_array = $ex_set_test_array->fetch_assoc())
 			{
 					$set_array=Set::find_by_id($test_set_array['id']);
-
-					echo "<strong>The id of the set object is : </strong>";
-					echo $test_set->id;
-					echo "<br>";
-					echo "<strong>The id that was queried was  : </strong>";
-					echo $set_array->id;
 					if(($set_array->id)==($test_set->id))
 					{
 						$pass_trigger=1;
@@ -56,10 +51,23 @@ include('header.html');
 
 			}
 
+		if($pass_trigger==1)
+		{
+			echo "<div class='well' style='background-color: #00ff00'>";
+				echo "<strong>Passed</strong>";
+				echo "</div>";
+		}
+		else
+		{
+			echo "<div class='well' style='background-color: #ff0000'>";
+				echo "<strong>Failed</strong>";
+				echo "</div>";
+		}
+
+
 	</xmp>
 </div>
-<p>Result:</p>
-<div class="well" style="background-color: #e6f7ff;">
+
 	<?php
 
 		    $rout_ex_set = new Routine();
@@ -95,12 +103,6 @@ include('header.html');
 	    	while($test_set_array = $ex_set_test_array->fetch_assoc())
 			{
 					$set_array=Set::find_by_id($test_set_array['id']);
-
-					echo "<strong>The id of the set object is : </strong>";
-					echo $test_set->id;
-					echo "<br>";
-					echo "<strong>The id that was queried was  : </strong>";
-					echo $set_array->id;
 					if(($set_array->id)==($test_set->id))
 					{
 						$pass_trigger=1;
@@ -108,24 +110,125 @@ include('header.html');
 
 			}
 
-	?>
-	</div>
-	<p>Status:</p>
-	<div class="well" style="background-color: #e6f7ff;">
-	<?php
-
 		if($pass_trigger==1)
 		{
-			echo "<strong>Passed</strong>";
+			echo "<div class='well' style='background-color: #00ff00'>";
+				echo "<strong>Passed</strong>";
+				echo "</div>";
 		}
 		else
 		{
-			echo "<strong>Failed</strong>";
+			echo "<div class='well' style='background-color: #ff0000'>";
+				echo "<strong>Failed</strong>";
+				echo "</div>";
 		}
 
+	?>
+
+
+
+
+<h3 class="expand sub-header">Test for Displaying Exercise Set</h3>
+
+<div class="well" style="display:none;">
+<xmp>
+
+
+
+   			global $database;
+			$sql = "SELECT * FROM wb_exercise_set WHERE routine_id=".$rout_ex_set->id." AND exercise_id=".$ex_set->id;
+			$ex_set_test_array = $database->query($sql);
+			$pass_trigger=1;
+	    	while($test_set_array = $ex_set_test_array->fetch_assoc())
+			{
+					$set_array=Set::find_by_id($test_set_array['id']);
+
+					if(($set_array->exercise_id)!=($test_set->exercise_id))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->routine_id)!=($test_set->routine_id))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->order)!=($test_set->order))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->reps)!=($test_set->reps))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->weight)!=($test_set->weight))
+					{
+						$pass_trigger=0;
+					}
+
+
+			}
+
+			if($pass_trigger==1)
+			{
+				echo "<strong>Passed</strong>";
+			}
+			else
+			{
+				echo "<strong>Failed</strong>";
+			}
+
+	</xmp>
+</div>
+
+	<?php
+
+		   global $database;
+			$sql = "SELECT * FROM wb_exercise_set WHERE routine_id=".$rout_ex_set->id." AND exercise_id=".$ex_set->id;
+			$ex_set_test_array = $database->query($sql);
+			$pass_trigger=1;
+	    	while($test_set_array = $ex_set_test_array->fetch_assoc())
+			{
+					$set_array=Set::find_by_id($test_set_array['id']);
+
+					if(($set_array->exercise_id)!=($test_set->exercise_id))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->routine_id)!=($test_set->routine_id))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->order)!=($test_set->order))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->reps)!=($test_set->reps))
+					{
+						$pass_trigger=0;
+					}
+					if(($set_array->weight)!=($test_set->weight))
+					{
+						$pass_trigger=0;
+					}
+
+
+			}
+
+			if($pass_trigger==1)
+			{
+				echo "<div class='well' style='background-color: #00ff00'>";
+				echo "<strong>Passed</strong>";
+				echo "</div>";
+			}
+			else
+			{
+				echo "<div class='well' style='background-color: #ff0000'>";
+				echo "<strong>Failed</strong>";
+				echo "</div>";
+			}
 
 	?>
-	</div>
+
+
 
 
 
