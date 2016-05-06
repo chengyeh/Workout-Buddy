5b06fd3fd241f45e9a9cf6110dddec70a0274173
Paul Charles <santosh@ku.edu>
2016-05-06 05:20:03 -0500

    Update testing

--- test/exercise_test.php
+++ test/exercise_test.php
@@ -7,30 +7,28 @@ include('header.html');
 ?>
 <!-- html goes here -->
 <h1 class="page-header">Exercise Testing</h1>
-<h3 class="expand sub-header">Test for showing number of exercises</h3>
+<h3 class="expand sub-header">Test for querying number of exercise types: show_types()</h3>
 
 <div class="well" style="display:none;">
 <xmp>
+//This test is to make sure the number of types is queried in wb_exercise_type table
 
+//The function that utilizes this test is:
+public function show_types()
+{
+	$sql = "SELECT * FROM wb_exercise_type";
+	$types_object_array = Types::find_by_sql($sql);
+	return $types_object_array;
+}
+$a=1;
+$var_types = Types::find_by_id(1);
+$display_types=$var_types->show_types();
+foreach($display_types as $display_feature)
+{
+	$a=$a+1;
+}
 
-
-			public function show_types()
-		   {
-		    	$sql = "SELECT * FROM wb_exercise_type";
-		    	$types_object_array = Types::find_by_sql($sql);
-		    	return $types_object_array;
-		    }
-		    $a=1;
-			$var_types = Types::find_by_id(1);
-			$display_types=$var_types->show_types();
-			foreach($display_types as $display_feature)
-			{
-
-					$a=$a+1;
-
-			}
-
-			//Since there are 285 exercises to choose from in the database table wb_exercise_type, the output should be 285 for $a
+//Since there are 285 exercises to choose from in the database table wb_exercise_type, the output should be 285 for $a
 
 	</xmp>
 </div>
@@ -63,41 +61,43 @@ include('header.html');
 	?>
 
 
-<h3 class="expand sub-header">est for Displaying the Right Name for an Exercise</h3>
+<h3 class="expand sub-header">Test for obtaining right fields from exercise types</h3>
 <div class="well" style="display:none;">
 <xmp>
-			$a=1;
-			$var_types = Types::find_by_id(1);
-			$display_types=$var_types->show_types();
-			//The 10th exercise in the database is Barbell Deadlifts. As a result, this test will see if it will display the name of that correctly
-			$echo_display_name;
-			foreach($display_types as $display_feature)
-			{
 
-					if($a==10)
-					{
-						echo "<strong>".$display_feature->name."</strong>";
+//This test is to make sure the right fields are being displayed and queried from the wb_exercise_types table
+$a=1;
+$var_types = Types::find_by_id(1);
+$display_types=$var_types->show_types();
+//The 10th exercise in the database is Barbell Deadlifts. As a result, this test will see if it will display the name of that correctly
+$echo_display_name;
+foreach($display_types as $display_feature)
+{
 
-						$echo_display_name=$display_feature->name;
-						$reference_id=$display_feature->id;
-					}
-					$a=$a+1;
-
-			}
-
-			$test_name="Barbell Deadlifts";
-			if(strcasecmp($echo_display_name,$test_name)==0)
-			{
-				echo "<div class='well' style='background-color: #b3ffcc'>";
-						echo "<strong>PASSED</strong>";
-						echo "</div>";
-		}
-		else
+		if($a==10)
 		{
-			echo "<div class='well' style='background-color: #ffd6cc'>";
-						echo "<strong>FAILED</strong>";
-						echo "</div>";
+			echo "<strong>".$display_feature->name."</strong>";
+
+			$echo_display_name=$display_feature->name;
+			$reference_id=$display_feature->id;
 		}
+		$a=$a+1;
+
+}
+
+$test_name="Barbell Deadlifts";
+if(strcasecmp($echo_display_name,$test_name)==0)
+{
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+			echo "<strong>PASSED</strong>";
+			echo "</div>";
+}
+else
+{
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+				echo "<strong>FAILED</strong>";
+				echo "</div>";
+}
 	</xmp>
 </div>
 
@@ -142,58 +142,58 @@ include('header.html');
 
 
 
-<h3 class="expand sub-header">Test for Adding an Exercise to a Routine</h3>
+<h3 class="expand sub-header">Test for adding an exercise to a routine</h3>
 <div class="well" style="display:none;">
 <xmp>
 
+//This test involves adding an exercise to a routine
+
+$rout_ex = new Routine();
+$rout_ex->user_id = 19;
+$rout_ex->name = "Test Routine for Exercise";
+$rout_ex->description = "This Routine is for Testing Purposes";
+$rout_ex->mon = 1;
+$rout_ex->start_date = '2016-05-01';
+$rout_ex->end_date = '2016-06-01';
+$rout_ex->create();
 
 
-			$rout_ex = new Routine();
-		    $rout_ex->user_id = 19;
-		    $rout_ex->name = "Test Routine for Exercise";
-		    $rout_ex->description = "This Routine is for Testing Purposes";
-		   	$rout_ex->mon = 1;
-		    $rout_ex->start_date = '2016-05-01';
-		    $rout_ex->end_date = '2016-06-01';
-			$rout_ex->create();
+$ex=new Exercises();
+$ex->routine_id=$rout_ex->id;
+$ex->type=$reference_id;
+$ex->create();
 
+global $database;
+$sql = "SELECT * FROM wb_exercise WHERE routine_id=".$rout_ex->id;
+$ex_rout_test_array = $database->query($sql);
 
-			$ex=new Exercises();
-			$ex->routine_id=$rout_ex->id;
-			$ex->type=$reference_id;
-			$ex->create();
+while($ex_rout_array = $ex_rout_test_array->fetch_assoc())
+{
+	$test_array=Exercises::find_by_id($ex_rout_array['id']);
 
-			global $database;
-			$sql = "SELECT * FROM wb_exercise WHERE routine_id=".$rout_ex->id;
-			$ex_rout_test_array = $database->query($sql);
+	echo "<strong>The id of the routine : </strong>";
+	echo "<br>";
+	echo $rout_ex->id;
+	echo "<br>";
+	echo "<strong>The routine_id for the exercise that was queried for the designated routine was: </strong>";
+	echo "<br>";
+	echo $test_array->routine_id;
+	echo "<br>";
 
-	    	while($ex_rout_array = $ex_rout_test_array->fetch_assoc())
-			{
-					$test_array=Exercises::find_by_id($ex_rout_array['id']);
+	if($test_array->routine_id==$rout_ex->id)
+	{
+		echo "<div class='well' style='background-color: #b3ffcc'>";
+		echo "<strong>PASSED</strong>";
+		echo "</div>";
+	}
+	else
+	{
+		echo "<div class='well' style='background-color: #ffd6cc'>";
+					echo "<strong>FAILED</strong>";
+					echo "</div>";
+	}
 
-					echo "<strong>The id of the routine : </strong>";
-					echo "<br>";
-					echo $rout_ex->id;
-					echo "<br>";
-					echo "<strong>The routine_id for the exercise that was queried for the designated routine was: </strong>";
-					echo "<br>";
-					echo $test_array->routine_id;
-					echo "<br>";
-
-					if($test_array->routine_id==$rout_ex->id)
-					{
-						echo "<div class='well' style='background-color: #b3ffcc'>";
-						echo "<strong>PASSED</strong>";
-						echo "</div>";
-					}
-					else
-					{
-						echo "<div class='well' style='background-color: #ffd6cc'>";
-									echo "<strong>FAILED</strong>";
-									echo "</div>";
-					}
-
-			}
+}
 	</xmp>
 </div>
 
@@ -245,37 +245,106 @@ include('header.html');
 
 
 
-	<h3 class="expand sub-header">Test for Deleting Exercises</h3>
+	<h3 class="expand sub-header">Test for querying exercises from routines: get_exercises()</h3>
 <div class="well" style="display:none;">
 <xmp>
+//This test involves using the get_exercises() function
+
+//The function used for this test is:
+ public function get_exercises()
+{
+	$sql = "SELECT * FROM wb_exercise WHERE routine_id=".$this->id. " ORDER BY id ASC";
+	$exercises_object_array = Exercises::find_by_sql($sql);
+	return $exercises_object_array;
+}
+$ex_rout_test_array = $rout_ex->get_exercises();
+$pass_trigger=0;
+foreach($ex_rout_test_array as $value)
+{
+		if($value->id==$ex->id)
+		{
+			$pass_trigger=1;
+		}
+}
+
+if($pass_trigger==1)
+{
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+	echo "<strong>PASSED</strong>";
+	echo "</div>";
+}
+else
+{
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+	echo "<strong>FAILED</strong>";
+	echo "</div>";
+}
+
+}
+	</xmp>
+</div>
+
+	<?php
 
 
 
-			$database->query("DELETE FROM wb_exercise WHERE routine_id = '" . $rout_ex->id . "'");
-		$rout_ex->delete();
 
-		global $database;
-			$sql = "SELECT * FROM wb_exercise WHERE routine_id=".$rout_ex->id;
-			$ex_rout_test_array = $database->query($sql);
-			$a=0;
-			foreach ($ex_rout_test_array as $ex_object)
+
+			$ex_rout_test_array = $rout_ex->get_exercises();
+			$pass_trigger=0;
+	    	foreach($ex_rout_test_array as $value)
 			{
-
-				$a=$a+1;
-
+					if($value->id==$ex->id)
+					{
+						$pass_trigger=1;
+					}
 			}
-			if($a == 0)
+
+			if($pass_trigger==1)
 			{
 				echo "<div class='well' style='background-color: #b3ffcc'>";
-						echo "<strong>PASSED</strong>";
-						echo "</div>";
-					}
-					else
-					{
-						echo "<div class='well' style='background-color: #ffd6cc'>";
-									echo "<strong>FAILED</strong>";
-									echo "</div>";
-					}
+		echo "<strong>PASSED</strong>";
+		echo "</div>";
+			}
+			else
+			{
+				echo "<div class='well' style='background-color: #ffd6cc'>";
+					echo "<strong>FAILED</strong>";
+					echo "</div>";
+			}
+
+	?>
+
+
+
+	<h3 class="expand sub-header">Test for deleting exercises</h3>
+<div class="well" style="display:none;">
+<xmp>
+//This test demonstrates the deleting of an exercise from the database
+
+$database->query("DELETE FROM wb_exercise WHERE routine_id = '" . $rout_ex->id . "'");
+$rout_ex->delete();
+
+global $database;
+$sql = "SELECT * FROM wb_exercise WHERE routine_id=".$rout_ex->id;
+$ex_rout_test_array = $database->query($sql);
+$a=0;
+foreach ($ex_rout_test_array as $ex_object)
+{
+	$a=$a+1;
+}
+if($a == 0)
+{
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+			echo "<strong>PASSED</strong>";
+			echo "</div>";
+}
+else
+{
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+				echo "<strong>FAILED</strong>";
+				echo "</div>";
+}
 	</xmp>
 </div>
 
--- test/message_test.php
+++ test/message_test.php
@@ -8,86 +8,85 @@ include('header.html');
 <!-- html goes here -->
 <h1 class="page-header">Message Testing</h1>
 
-<h3 class="expand sub-header">Test for send message</h3>
+<h3 class="expand sub-header">Test to send message</h3>
 <div class="well" style="display:none;">
 <xmp>
 
+//This test will test if a message can be sent from a user.
+//Following code will test whether it can send a query from a designated message.
+date_default_timezone_set('America/Chicago');
+$dt = new DateTime();
+$tz = new DateTimeZone('America/Chicago');
+$pm = new Message();
+$pm->user = $trimmed['user_id'];
+//Have the $user_id=5
+$pm->receiver = $view_user->id;
+//Have the $view_user->id = 9
+$pm->message = $trimmed['message'];
+//Let's have the message be: "Let's play tennis on Friday!"
+$pm->Date = $dt->format('m-d-Y');
+//Date will be today's date
+$pm->Time = $dt->format('H:i:s');
+//Time will be the immediate time
+$pm->create();
 
-	//This test will test if a message can be sent from a user.
-	//Following code will test whether it can send a query from a designated message.
-	date_default_timezone_set('America/Chicago');
-    $dt = new DateTime();
-	$tz = new DateTimeZone('America/Chicago');
-	$pm = new Message();
-    $pm->user = $trimmed['user_id'];
-    //Have the $user_id=5
-    $pm->receiver = $view_user->id;
-    //Have the $view_user->id = 9
-    $pm->message = $trimmed['message'];
-    //Let's have the message be: "Let's play tennis on Friday!"
-	$pm->Date = $dt->format('m-d-Y');
-	//Date will be today's date
-    $pm->Time = $dt->format('H:i:s');
-    //Time will be the immediate time
-    $pm->create();
+date_default_timezone_set('America/Chicago');
+$dt = new DateTime();
+$tz = new DateTimeZone('America/Chicago');
 
-    date_default_timezone_set('America/Chicago');
-    $dt = new DateTime();
-	$tz = new DateTimeZone('America/Chicago');
-
-	$pm = new Message();
+$pm = new Message();
 
 
-    $pm->user = 5;
-    $pm->receiver = 9;
-    $pm->message = "Let's play tennis on Friday!";
-    $pm->Date = $dt->format('m-d-Y');
-    $pm->Time = $dt->format('H:i:s');
-    $pm->create();
+$pm->user = 5;
+$pm->receiver = 9;
+$pm->message = "Let's play tennis on Friday!";
+$pm->Date = $dt->format('m-d-Y');
+$pm->Time = $dt->format('H:i:s');
+$pm->create();
 
-	global $database;
-    	$sql = "SELECT * FROM wb_messages WHERE id=".$pm->id;
-    	$group_member_array = $database->query($sql);
-    	$pass_trigger=1;
-    	while($log_array = $group_member_array->fetch_assoc())
-		{
+global $database;
+$sql = "SELECT * FROM wb_messages WHERE id=".$pm->id;
+$group_member_array = $database->query($sql);
+$pass_trigger=1;
+while($log_array = $group_member_array->fetch_assoc())
+{
 
-					$set_array=Message::find_by_id($log_array['id']);
-					if(($set_array->user)!=$pm->user)
-					{
-						$pass_trigger=0;
-					}
+	$set_array=Message::find_by_id($log_array['id']);
+	if(($set_array->user)!=$pm->user)
+	{
+		$pass_trigger=0;
+	}
 
-					if(($set_array->receiver)!=$pm->receiver)
-					{
-						$pass_trigger=0;
-					}
-					if(($set_array->message)!=$pm->message)
-					{
-						$pass_trigger=0;
-					}
-					if(($set_array->Date)!=$pm->Date)
-					{
-						$pass_trigger=0;
-					}
-					if(($set_array->Time)!=$pm->Time)
-					{
-						$pass_trigger=0;
-					}
+	if(($set_array->receiver)!=$pm->receiver)
+	{
+		$pass_trigger=0;
+	}
+	if(($set_array->message)!=$pm->message)
+	{
+		$pass_trigger=0;
+	}
+	if(($set_array->Date)!=$pm->Date)
+	{
+		$pass_trigger=0;
+	}
+	if(($set_array->Time)!=$pm->Time)
+	{
+		$pass_trigger=0;
+	}
 
-		}
-		if($pass_trigger==1)
-		{
-			echo "<div class='well' style='background-color: #b3ffcc'>";
-				echo "<strong>PASSED</strong>";
-				echo "</div>";
-		}
-		else
-		{
-			echo "<div class='well' style='background-color: #ffd6cc'>";
-				echo "<strong>FAILED</strong>";
-				echo "</div>";
-		}
+}
+if($pass_trigger==1)
+{
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+		echo "<strong>PASSED</strong>";
+		echo "</div>";
+}
+else
+{
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+		echo "<strong>FAILED</strong>";
+		echo "</div>";
+}
 
 	</xmp>
 </div>
@@ -164,50 +163,50 @@ include('header.html');
 
 <div class="well" style="display:none;">
 	<xmp>
-	  	global $database;
-    	$a=0;
-    	$sql2 = "SELECT * FROM wb_messages WHERE receiver=".$pm->receiver." AND del_receive=".$a;
-    	$group_message_array = $database->query($sql2);
-      	$pass_trigger=1;
-    	while($log_array = $group_message_array->fetch_assoc())
-		{
-				if($log_array['id']==$pm->id)
-				{
-					$set_array=Message::find_by_id($log_array['id']);
-					$v=User::find_by_id($log_array['user']);
-					if(($set_array->message)!=$pm->message)
-					{
-						$pass_trigger=0;
-					}
+//This test demonstrates the user receiving messages
 
-					if(($set_array->receiver)!=$pm->receiver)
-					{
-						$pass_trigger=0;
-					}
-					if(($set_array->Date)!=$pm->Date)
-					{
-						$pass_trigger=0;
-					}
-					if(($set_array->Time)!=$pm->Time)
-					{
-						$pass_trigger=0;
-					}
-				}
-
-
-		}
-		if($pass_trigger==1)
+global $database;
+$a=0;
+$sql2 = "SELECT * FROM wb_messages WHERE receiver=".$pm->receiver." AND del_receive=".$a;
+$group_message_array = $database->query($sql2);
+$pass_trigger=1;
+while($log_array = $group_message_array->fetch_assoc())
+{
+	if($log_array['id']==$pm->id)
+	{
+		$set_array=Message::find_by_id($log_array['id']);
+		$v=User::find_by_id($log_array['user']);
+		if(($set_array->message)!=$pm->message)
 		{
-			echo "<div class='well' style='background-color: #b3ffcc'>";
-				echo "<strong>PASSED</strong>";
-				echo "</div>";
+			$pass_trigger=0;
 		}
-		else
+
+		if(($set_array->receiver)!=$pm->receiver)
 		{
-			echo "<div class='well' style='background-color: #ffd6cc'>";
-				echo "<strong>FAILED</strong>";
-				echo "</div>";
+			$pass_trigger=0;
 		}
+		if(($set_array->Date)!=$pm->Date)
+		{
+			$pass_trigger=0;
+		}
+		if(($set_array->Time)!=$pm->Time)
+		{
+			$pass_trigger=0;
+		}
+	}
+}
+if($pass_trigger==1)
+{
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+		echo "<strong>PASSED</strong>";
+		echo "</div>";
+}
+else
+{
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+		echo "<strong>FAILED</strong>";
+		echo "</div>";
+}
 
 
 	</xmp>
@@ -267,39 +266,37 @@ include('header.html');
 
 <div class="well" style="display:none;">
 	<xmp>
-	  global $database;
-		$database->query("UPDATE wb_messages SET del_receive = 1 WHERE id = ".$pm->id);
+//This test demonstrates the deletion of messages using markers
 
+global $database;
+$database->query("UPDATE wb_messages SET del_receive = 1 WHERE id = ".$pm->id);
+global $database;
+$sql = "SELECT * FROM wb_messages WHERE receiver=".$pm->receiver;
+$group_member_array = $database->query($sql);
+$del_rec_trans;
+while($log_array = $group_member_array->fetch_assoc())
+{
+	if($log_array['id']==$pm->id)
+	{
+		$set_array=Message::find_by_id($log_array['id']);
+		$del_rec_trans=$set_array->del_receive;
 
+	}
 
-		global $database;
-    	$sql = "SELECT * FROM wb_messages WHERE receiver=".$pm->receiver;
-    	$group_member_array = $database->query($sql);
-    	$del_rec_trans;
-    	while($log_array = $group_member_array->fetch_assoc())
-		{
-				if($log_array['id']==$pm->id)
-				{
-					$set_array=Message::find_by_id($log_array['id']);
-					$del_rec_trans=$set_array->del_receive;
+}
 
-				}
-
-		}
-
-
-		if(($del_rec_trans)==1)
-		{
-			echo "<div class='well' style='background-color: #b3ffcc'>";
-				echo "<strong>PASSED</strong>";
-				echo "</div>";
-		}
-		else
-		{
-			echo "<div class='well' style='background-color: #ffd6cc'>";
-				echo "<strong>FAILED</strong>";
-				echo "</div>";
-		}
+if(($del_rec_trans)==1)
+{
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+		echo "<strong>PASSED</strong>";
+		echo "</div>";
+}
+else
+{
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+		echo "<strong>FAILED</strong>";
+		echo "</div>";
+}
 
 
 	</xmp>
@@ -346,43 +343,41 @@ include('header.html');
 
 <div class="well" style="display:none;">
 	<xmp>
-	  global $database;
-		$database->query("UPDATE wb_messages SET del_sent = 1 WHERE id = ".$pm->id);
+//This test demonstrates deleting messages that the user sent using markers
 
-    	echo "<strong>The del_sent value is: </strong>";
+global $database;
+$database->query("UPDATE wb_messages SET del_sent = 1 WHERE id = ".$pm->id);
 
-		global $database;
-    	$sql = "SELECT * FROM wb_messages WHERE receiver=".$pm->receiver;
-    	$group_member_array = $database->query($sql);
-    	$del_sent_trans;
-    	while($log_array = $group_member_array->fetch_assoc())
-		{
-				if($log_array['id']==$pm->id)
-				{
-					$set_array=Message::find_by_id($log_array['id']);
-					$del_sent_trans=$set_array->del_sent;
-					echo $set_array->del_sent;
-				}
+echo "<strong>The del_sent value is: </strong>";
 
-		}
+global $database;
+$sql = "SELECT * FROM wb_messages WHERE receiver=".$pm->receiver;
+$group_member_array = $database->query($sql);
+$del_sent_trans;
+while($log_array = $group_member_array->fetch_assoc())
+{
+	if($log_array['id']==$pm->id)
+	{
+		$set_array=Message::find_by_id($log_array['id']);
+		$del_sent_trans=$set_array->del_sent;
+		echo $set_array->del_sent;
+	}
 
-		echo "<br>";
-		if(($del_sent_trans)==1)
-		{
-			echo "<div class='well' style='background-color: #b3ffcc'>";
-				echo "<strong>PASSED</strong>";
-				echo "</div>";
-		}
-		else
-		{
-			echo "<div class='well' style='background-color: #ffd6cc'>";
-				echo "<strong>FAILED</strong>";
-				echo "</div>";
-		}
+}
 
-
-
-
+echo "<br>";
+if(($del_sent_trans)==1)
+{
+	echo "<div class='well' style='background-color: #b3ffcc'>";
+		echo "<strong>PASSED</strong>";
+		echo "</div>";
+}
+else
+{
+	echo "<div class='well' style='background-color: #ffd6cc'>";
+		echo "<strong>FAILED</strong>";
+		echo "</div>";
+}
 
 	</xmp>
 </div>
@@ -424,36 +419,35 @@ include('header.html');
 		}
 	?>
 
-<h3 class="expand sub-header">Test for deleting messages from sent</h3>
+<h3 class="expand sub-header">Test for displaying inbox icon</h3>
 
 <div class="well" style="display:none;">
 	<xmp>
+//This test demonstrates displaying an inbox icon in the navigation bar using markers
 
-		$database->query("UPDATE `wb_messages` SET `read_message`=1 WHERE `id`=".$pm->id);
-
-
-	    global $database;
-    	$sql = "SELECT * FROM wb_messages WHERE id=".$pm->id;
-    	$group_member_array = $database->query($sql);
-	    while($log_array = $group_member_array->fetch_assoc())
+$database->query("UPDATE `wb_messages` SET `read_message`=1 WHERE `id`=".$pm->id);
+global $database;
+$sql = "SELECT * FROM wb_messages WHERE id=".$pm->id;
+$group_member_array = $database->query($sql);
+while($log_array = $group_member_array->fetch_assoc())
+{
+	if($log_array['id']==$pm->id)
+	{
+		$set_array=Message::find_by_id($log_array['id']);
+		if(($set_array->read_message)==1)
 		{
-				if($log_array['id']==$pm->id)
-				{
-					$set_array=Message::find_by_id($log_array['id']);
-					if(($set_array->read_message)==1)
-					{
-						echo "<div class='well' style='background-color: #b3ffcc'>";
-						echo "<strong>PASSED</strong>";
-						echo "</div>";
-					}
-					else
-					{
-						echo "<div class='well' style='background-color: #ffd6cc'>";
-						echo "<strong>FAILED</strong>";
-						echo "</div>";
-					}
-				}
+			echo "<div class='well' style='background-color: #b3ffcc'>";
+			echo "<strong>PASSED</strong>";
+			echo "</div>";
 		}
+		else
+		{
+			echo "<div class='well' style='background-color: #ffd6cc'>";
+			echo "<strong>FAILED</strong>";
+			echo "</div>";
+		}
+	}
+}
 	</xmp>
 </div>
 
